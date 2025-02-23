114456eaabce5f5f8d6d497b7a7561334a746d23
hakurei77 <hakurei77@foxmail.com>
2025-02-23 18:29:07 +0800

    11

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+export const apps = [
+  {
+    name: "my-next-app",
+    script: "npm",
+    args: "start",
+    cwd: "/www/wwwroot/hakurei77-llm-project",
+    env: {
+      NODE_ENV: "production",
+    },
+  },
+];
