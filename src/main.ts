7bf3def6aa0010079a33c40e66eda258ac1d967d
hakurei77 <hakurei77@foxmail.com>
2025-02-25 00:11:29 +0800

    7422test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4410);
+  await app.listen(7422);
 }
 bootstrap();
