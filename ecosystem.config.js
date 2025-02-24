9ba2ee6addb6eb30bac4bc31692c5a1d1ec90d51
hakurei77 <hakurei77@foxmail.com>
2025-02-24 13:10:04 +0800

    t'ttt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = [
   {
     name: "my-next-app",
-    script: "dist/main.js",
+    script: "npm",
+    args: "run start:prod",
     cwd: "/www/wwwroot/hakurei77-llm-project",
     env: {
       NODE_ENV: "production",
